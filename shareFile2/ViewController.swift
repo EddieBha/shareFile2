453dc7ffbf34ec776daed4cf52db343443df5947
eddieage <ekyrk@hotmail.com>
2019-01-21 14:40:19 +0100

    Initial Commit

---
+++ shareFile2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shareFile2
+//
+//  Created by Eddie Agegnehu Kyrk on 2019-01-21.
+//  Copyright © 2019 Eddie Agegnehu Kyrk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
